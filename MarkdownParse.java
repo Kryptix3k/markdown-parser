8ca3d865637e738a88a761987f88af4d52d4fd1b
Kryptix3k <102953250+Kryptix3k@users.noreply.github.com>
2022-04-14 13:40:53 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Currency;
 
 public class MarkdownParse {
 
@@ -13,15 +14,19 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
+            if(openBracket < currentIndex) {
+                break;
+            }
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
         return toReturn;
-    }
+        }
+    
+
 
 
     public static void main(String[] args) throws IOException {
@@ -31,3 +36,4 @@ public class MarkdownParse {
 	    System.out.println(links);
     }
 }
+
