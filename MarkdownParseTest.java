c49ea6e583f161ed3fb7eab260a0689dfc18f8db
Kryptix3k <102953250+Kryptix3k@users.noreply.github.com>
2022-05-25 02:17:10 -0700

    wfw

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -42,4 +42,22 @@ public class MarkdownParseTest {
        assertEquals(List.of("google.com"), expectedLinks);
     }
 
+    @Test
+    public void testSecondSnippet() throws IOException {
+        MarkdownParse markDown = new MarkdownParse();
+        Path directory = Path.of("snippet2.md");
+        String content = Files.readString(directory);
+       ArrayList expectedLinks = markDown.getLinks(content);
+       assertEquals(List.of("b.com", "a.com(())", "example.com"), expectedLinks);
+    }
+
+    @Test
+    public void testThirdSnippet() throws IOException {
+        MarkdownParse markDown = new MarkdownParse();
+        Path directory = Path.of("snippet3.md");
+        String content = Files.readString(directory);
+       ArrayList expectedLinks = markDown.getLinks(content);
+       assertEquals(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"), expectedLinks);
+    }
+
 }
---
+++ vim.java
@@ -0,0 +1,8 @@
+public class vim {
+    public static void main(String args[]) {
+        int i = 8;
+
+        System.out.print(i*i);
+        i=6;
+    }
+}
